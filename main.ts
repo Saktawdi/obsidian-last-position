398b82143f58b84534d74ddd9fa01f9d3036d4e6
Sakta_wdi <1742804029@qq.com>
2025-01-27 22:52:20 +0800

    修复bug

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, } from 'obsidian'
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, WorkspaceLeaf, } from 'obsidian'
 interface MyPluginSettings {
 	//自动保存间隔时间,单位秒
 	myInterval: number;
-	//首次打开延迟跳转时间，单位秒
-	myDelay:number;
+	//重试策略-重试次数最大值
+	myRetryCount: number;
 	//数据
 	scrollHeightData: Map<string, number | undefined>;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
 	myInterval: 3,
-	myDelay: 1,
+	myRetryCount: 100,
 	//数据
 	scrollHeightData: new Map<string, number>(),
 }
@@ -23,55 +23,78 @@ export default class MyPlugin extends Plugin {
 	
 	async onload() {
 		await this.loadSettings();
-		console.log("版本号：：：：v1.0.0.1")
-		// 等待Obsidian完全加载
+		// 等待Obsidian加载
 		this.app.workspace.onLayoutReady(() => {
 			// 获取当前活动的Markdown视图
 			this.view = this.app.workspace.getActiveViewOfType(MarkdownView);
 			//获取当前文件信息，并且监听打开，并且跳转视图
 			this.readOpenFileInfo();
+
 			//注册时钟，每x秒钟就自动保存当前文件高度
 			this.registerInterval(window.setInterval(() =>{
 				this.settings.scrollHeightData.set(this.fileName,this.scrollHeight);
 				this.saveSettings();
 			},this.settings.myInterval * 1000));
+
 		});
 		// 右下角状态栏
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText(`当前高度:${this.scrollHeight}`)
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
+
+
 		this.registerDomEvent(document, "mouseover", (ev) => {
-			this.scrollHeight = this.view?.previewMode.getScroll();
+			this.scrollHeight = this.view?.currentMode.getScroll();
 			statusBarItemEl.setText(`当前高度: ${this.scrollHeight?.toFixed(0)}`);
 		})
+
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
+
+
 	async readOpenFileInfo() {
-		if (this.view) {
+		if (!this.view) {
+			console.warn("当前没有活动的 Markdown 视图");
+			return;
+		}else{
 			const file = this.view.file; // 获取当前文件对象
 			if (!file) throw new Error("获取当前文件对象失败")
 			this.fileName = file.basename; // 获取文件名
-			const delay = 300 + (file.stat.size/10/1024);
-			this.previewScrollTO(delay + this.settings.myDelay * 1000);
+			this.previewScrollTO();
 		}
 		this.app.workspace.on("file-open", async (file) => {
 			if (!file) throw new Error("获取文件对象失败")
 			this.fileName = file.basename; // 更新文件名
 			// 更新当前活动的Markdown视图
 			this.view = this.app.workspace.getActiveViewOfType(MarkdownView);
-			const delay = 300 + (file.stat.size/10/1024);
-			this.previewScrollTO(delay);
+			this.previewScrollTO();
 		});
 	}
 
-	async previewScrollTO(delay:number){
+	/**
+	 * 采用重试策略
+	 */
+	async previewScrollTO(){
 		const lastHeight = this.settings.scrollHeightData.get(this.fileName);
 		if(lastHeight){
-			setTimeout(()=>{
-				this.view?.previewMode.applyScroll(lastHeight);
-			},delay)
+			let retryCount  = 0;
+			const maxRetries = this.settings.myRetryCount;
+			const retry = () => {
+				if (retryCount >= maxRetries) {
+					console.warn("重试次数达到上限，停止重试");
+					return;
+				}
+				retryCount ++;
+                if (this.scrollHeight !== lastHeight) {
+					console.warn("尝试重试",retryCount - 1)
+					setTimeout(retry, 100); // 每 100ms 重试一次
+                }
+				this.view?.currentMode.applyScroll(lastHeight);
+				this.scrollHeight = this.view?.currentMode.getScroll();
+            };
+            retry();
 		}
 	}
 
@@ -126,22 +149,6 @@ class SampleSettingTab extends PluginSettingTab {
 						await this.plugin.saveSettings(); // 保存设置
 					}
 				}));
-		//延迟时间设置
-		new Setting(containerEl)
-		.setName('首次延迟时间')
-		.setDesc('刚打开软件，对于默认文件执行跳转的时间，无效则往后延迟，单位为秒')
-		.addText((text) =>
-		text
-			.setPlaceholder('输入延迟时间（秒）')
-			.setValue(this.plugin.settings.myDelay.toString())
-			.onChange(async (value) => {
-				// 将输入的值转换为数字
-				const delay = Number(value);
-				if (!isNaN(delay) && delay > 0) {
-					this.plugin.settings.myDelay = delay;
-					await this.plugin.saveSettings(); // 保存设置
-				}
-			}));
 		// 展示 scrollHeightData 数据
         new Setting(containerEl)
             .setName('文件滚动高度数据')
